1a740a4892d878c1c6d4fceb13b7b802f043dac9
Laura <lautzipa@gmail.com>
2020-01-27 15:04:21 +0200

    new input system package, basicMovement-script using inputs

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/InputManager.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputManager : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputManager()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputManager"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""d9095c42-2cdd-4d74-aa27-c87088e3fa0b"",
+            ""actions"": [
+                {
+                    ""name"": ""Walking"",
+                    ""type"": ""Value"",
+                    ""id"": ""e9f37b40-b6c9-4ca1-88f2-21fa6ed79d99"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""2c04dbdf-972e-4923-86df-0bd99389d644"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walking"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""71996102-721e-4864-8f16-0cbb4c8e2bf3"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walking"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""de9c6bb4-067a-4d28-971b-4bf0a2aded05"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walking"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""80567538-cb04-4f74-a682-e349aee9c915"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walking"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""0b76836c-3351-4df4-88c8-e70f5c546c9e"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walking"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Walking = m_Player.FindAction("Walking", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Walking;
+    public struct PlayerActions
+    {
+        private @InputManager m_Wrapper;
+        public PlayerActions(@InputManager wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Walking => m_Wrapper.m_Player_Walking;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Walking.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnWalking;
+                @Walking.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnWalking;
+                @Walking.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnWalking;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Walking.started += instance.OnWalking;
+                @Walking.performed += instance.OnWalking;
+                @Walking.canceled += instance.OnWalking;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnWalking(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/Player/BasicMovement.cs
+++ Assets/Scripts/Player/BasicMovement.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class BasicMovement : MonoBehaviour
+public class BasicMovement : MonoBehaviour, InputManager.IPlayerActions
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float speed;
+
+    // Input mappings:
+    private InputManager controls;
+
+
+    private void Awake()
     {
-        
+        // Luodaan inputtia kontrolloiva olio
+        controls = new InputManager();
+
+        // Kerrotaan DefaultInputs:lle, että tämä olio kuuntelee
+        // Player action map:iin liittyviä actioneita
+        controls.Player.SetCallbacks(this);
     }
 
-    // Update is called once per frame
+    private void Start()
+    {
+        // Ota kontrollit käyttöön
+        controls.Enable();
+    }
+
     void Update()
     {
+        Vector2 moveInput = controls.Player.Walking.ReadValue<Vector2>();
+        transform.Translate(moveInput * speed * Time.deltaTime);
+    }
+
+    #region InputHandling
+
+    public void OnWalking(InputAction.CallbackContext context)
+    {
         
     }
+
+    #endregion
 }
