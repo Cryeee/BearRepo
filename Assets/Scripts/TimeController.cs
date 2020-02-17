c6982b9bb3be2ad3880d9a01989494f604c85913
Jami <jami.salonen@hotmail.com>
2020-02-17 10:10:58 +0200

    aikajuttuja alotettu

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+
+    //show static variables in inspector
+    public float inspectorTime;
+    public float inspectorRoundTime;
+
+    public static float time = 0;
+    public static float roundTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetTimers(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = Time.time;
+    }
+
+    public void ResetTimers(float resetToAmount)
+    {
+        time = resetToAmount;
+        roundTime = resetToAmount;
+    }
+}
