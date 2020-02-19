71f25b5ecc7dd529c5cc088266a3c370127e0d0d
Jami <jami.salonen@hotmail.com>
2020-02-19 15:30:38 +0200

    Camera stuff
    custom physics removed

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public GameObject CMFreeLookCamera;
     public float AmountOfFoodEaten;
 
     public Animator playerAnimator;
@@ -17,6 +19,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         playerAnimator = gameObject.GetComponent<Animator>();
         //fatteningAnimation = playerAnimator.
     }
@@ -24,6 +27,14 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+            //TODO put these somewhere else =)
+            CMFreeLookCamera.GetComponent<CinemachineFreeLook>().m_Orbits[0].m_Radius = sizeIncrease * 5 + 5;
+            CMFreeLookCamera.GetComponent<CinemachineFreeLook>().m_Orbits[1].m_Radius = sizeIncrease * 7 + 7;
+            CMFreeLookCamera.GetComponent<CinemachineFreeLook>().m_Orbits[2].m_Radius = sizeIncrease * 5 + 5;
+
+        
+       // CMFreeLookCamera.GetComponent<CinemachineFreeLook>().m_Orbits[1].m_Radius = 20;
+
         sizeIncrease = AmountOfFoodEaten / 100;
 
         playerAnimator.Play("Fattening", 0, sizeIncrease);
