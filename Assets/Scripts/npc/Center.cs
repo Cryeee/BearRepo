93193eec0565a4fa9dbce6f8002047dedcf39d7e
oona <oona.liukkonen@gmail.com>
2020-03-09 15:03:45 +0200

    added navmesh to the map, better bird och fish

---
+++ Assets/Scripts/npc/Center.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Center : MonoBehaviour
+{
+    void Move()
+    {
+        transform.position += new Vector3(-1f, 0f, 0f);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Move();
+    }
+}
