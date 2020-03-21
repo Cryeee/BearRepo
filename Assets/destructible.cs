fc260963226314b4108c7a14cb44d74a0eb76015
Ganze <mikko.tanskanen93@gmail.com>
2020-03-21 12:34:41 +0200

    added breakable wall and glowing mushrooms to my test scene

---
+++ Assets/destructible.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destructible : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+
+   void OnMouseDown()
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
