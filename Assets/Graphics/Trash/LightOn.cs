7487995dda5d304b79cc0bf4e4f7b0eae1e17b3b
Niko <niko.sarkkomaa@tuni.fi>
2020-04-12 19:29:57 +0300

    Menu and level2 almost finished

---
+++ Assets/Graphics/Trash/LightOn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOn : MonoBehaviour
+{
+    public GameObject player;
+    public float playerYPos;
+    public Light spotLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        playerYPos = player.transform.position.y;
+
+        if(playerYPos < -20)
+        {
+            spotLight.intensity = Mathf.Lerp(spotLight.intensity, 1000, 0.02f);
+
+        }
+        else
+        {
+            spotLight.intensity = 0;
+        }
+    }
+}
