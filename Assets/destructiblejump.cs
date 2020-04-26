84b0e050973f1ea2b9b70606419def37cc304dd8
Niko <niko.sarkkomaa@tuni.fi>
2020-04-26 11:57:12 +0300

    Jump to tutorial level

---
+++ Assets/destructiblejump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destructiblejump : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+    public GameObject player;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K)) {
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if(other.gameObject.tag == "Player" && PlayerScript.inBallMode && player.GetComponent<Rigidbody>().velocity.y < -2)
+           if (other.gameObject.tag == "Player" && PlayerScript.inBallMode && PlayerScript.AmountOfFoodEaten >= 50 && player.GetComponent<Rigidbody>().velocity.y < -2)
+            {
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+
+    }
+}
