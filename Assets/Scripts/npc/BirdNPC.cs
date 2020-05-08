0291ca42f4c72687e32a9db4dea8201ad6dc04f5
Oona <oona.liukkonen@gmail.com>
2020-05-08 12:48:44 +0300

    wip bird

--- Assets/Scripts/npc/BirdNPC.cs
+++ Assets/Scripts/npc/BirdNPC.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class BirdNPC : MonoBehaviour
 {
-    public GameObject Player;
+    public GameObject fatBear;
+    public GameObject skinnyBear;
+    private GameObject player;
     public float fleeDistance = 4.0f;
     public float fleeTime = 1.0f;
     public float speed = 1f;
     bool spooked = false;
+    private Animator animator;
     
 
     void Fly()
     {
-        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        float distance = Vector3.Distance(transform.position, player.transform.position);
 
         if (distance < fleeDistance && !spooked)
         {
@@ -22,10 +25,14 @@ public class BirdNPC : MonoBehaviour
 
         if (spooked == true)
         {
-           // StartCoroutine(Wait());
-           
+            // StartCoroutine(Wait());
+            animator.SetBool("Fly", true);
 
             transform.position += new Vector3(0.03f, 0.1f * speed, 0f);
+        }
+        else
+        {
+            animator.SetBool("Fly", false);
         }
     }
 
@@ -39,8 +46,22 @@ public class BirdNPC : MonoBehaviour
         spooked = true;
     }
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
+        if (PlayerScript.inBallMode == false)
+        {
+            player = skinnyBear;
+        }
+        if (PlayerScript.inBallMode == true)
+        {
+            player = fatBear;
+        }
+
         Fly();
 
         if (transform.position.y > 25f)
